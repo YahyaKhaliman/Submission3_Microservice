3ed8ab7672908fb6b091da6adc566c2f7cd89d50
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-11-14 21:10:47 +0700

    Submission 3

--- Submission3/order-service/build_push_image_order_service.sh
+++ Submission3/order-service/build_push_image_order_service.sh
@@ -5,4 +5,4 @@ docker build -t ghcr.io/yahyakhaliman/order-service .
 echo $PAT | docker login ghcr.io --username yahyakhaliman --password-stdin 
 
 # Mengunggah image ke Github Container Registry
-docker push ghcr.io/yahyakhaliman/order-service 
+docker push ghcr.io/yahyakhaliman/order-service
